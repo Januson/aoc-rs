48277c45fae224a1d013103eb5d53ddabe6a2553
Ondrej Janus <janusonx@gmail.com>
2023-12-08 21:42:44 +0100

    Day 04 - Part 2: complete

--- src/day_04/scratchcards.rs
+++ src/day_04/scratchcards.rs
@@ -1,8 +1,9 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 
 #[derive(Debug)]
 struct Scratchcard {
+    id: u8,
     winning: HashSet<u8>,
     numbers: Vec<u8>,
 }
@@ -12,6 +13,7 @@ impl FromStr for Scratchcard {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parts: Vec<&str> = s.split([':', '|']).collect();
+        let id = parts[0].strip_prefix("Card ").unwrap().trim().parse::<u8>().unwrap();
         let winning: HashSet<u8> = parts[1].split(' ')
             .map(|number| number.trim())
             .filter(|number| !number.is_empty())
@@ -23,15 +25,19 @@ impl FromStr for Scratchcard {
             .map(|number| number.parse::<u8>().unwrap())
             .collect();
 
-        Ok(Scratchcard { winning, numbers })
+        Ok(Scratchcard { id, winning, numbers })
     }
 }
 
 impl Scratchcard {
-    fn score(&self) -> u16 {
-        let wins = self.numbers.iter()
+    fn wins(&self) -> u8 {
+        self.numbers.iter()
             .filter(|n| self.winning.contains(n))
-            .count();
+            .count() as u8
+    }
+
+    fn score(&self) -> u16 {
+        let wins = self.wins();
 
         if wins == 0 {
             0
@@ -40,6 +46,7 @@ impl Scratchcard {
                 .fold(1u16, |a, _| a * 2)
         }
     }
+
 }
 
 fn total_score(input: &str) -> u32 {
@@ -50,6 +57,27 @@ fn total_score(input: &str) -> u32 {
         .sum::<u32>()
 }
 
+
+fn total_cards(input: &str) -> u32 {
+    let cards: Vec<Scratchcard> = input.lines()
+        .map(|line| Scratchcard::from_str(line).unwrap())
+        .collect();
+    let mut card_count: HashMap<u8, u32> = HashMap::new();
+    for card in &cards {
+        card_count.insert(card.id, 1);
+    }
+
+    for card in &cards {
+        let held_cards = *card_count.get(&card.id).unwrap();
+        let wins = card.wins();
+        (1..=wins).into_iter()
+            .for_each(|n| {
+                card_count.entry(card.id + n).and_modify(|count| *count += 1u32 * held_cards);
+            });
+    }
+
+    card_count.values().sum::<u32>()
+}
 
 #[cfg(test)]
 mod tests {
@@ -65,7 +93,16 @@ mod tests {
     }
 
     #[test]
-    fn example() {
+    fn second_part() {
+        let input = include_str!("../../input/day_04/input.txt");
+
+        let total = total_cards(input);
+
+        assert_eq!(5037841, total);
+    }
+
+    #[test]
+    fn example_scores() {
         let input = "\
         Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
         Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
@@ -80,6 +117,22 @@ mod tests {
     }
 
     #[test]
+    fn example_cards() {
+        let input = "\
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n\
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n\
+        ";
+
+        let total = total_cards(input);
+
+        assert_eq!(30, total);
+    }
+
+    #[test]
     fn card_score() {
         let mut winning: HashSet<u8> = HashSet::new();
         winning.insert(41);
@@ -88,10 +141,12 @@ mod tests {
         winning.insert(86);
         winning.insert(17);
         let card = Scratchcard {
+            id: 1u8,
             winning,
             numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
         };
 
+        assert_eq!(4, card.wins());
         assert_eq!(8, card.score());
     }
 }
