46f748a9fbb312fb3e6388dc78a9fc08a4313474
Ondrej Janus <janusonx@gmail.com>
2024-12-01 08:56:39 +0100

    Repurpose repo for aoc-2024

--- src/day_07/mod.rs
+++
@@ -1,1 +0,0 @@
-mod camel_cards;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
 mod day_01;
-mod day_02;
-mod day_03;
-mod day_04;
-mod day_05;
-mod day_06;
-mod day_07;
-mod day_08;
-mod day_09;
-mod day_10;
-mod day_11;
-mod day_12;
 
 fn main() {
     println!("Hello, world!");
