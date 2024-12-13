a6f7519e7b48caaf1c8ff14b3cc333a8f55b2ee8
Ondrej Janus <janusonx@gmail.com>
2024-12-13 21:04:19 +0100

    Year 2015 - day 01 - part 2 solved

--- src/day_01/floor_counting.rs
+++ src/day_01/floor_counting.rs
@@ -9,15 +9,31 @@ impl Directions {
 
     fn final_floor(&self) -> i32 {
         let frequencies = frequencies(&self.0);
-        let up = frequencies.frequencies.get(&'(').unwrap_or(&0);
-        let down = frequencies.frequencies.get(&')').unwrap_or(&0);
+        let up = frequencies.get('(');
+        let down = frequencies.get(')');
 
-        (*up as i32) - (*down as i32)
+        (up as i32) - (down as i32)
+    }
+
+    fn basement(&self) -> i32 {
+        let reductions = reductions(0, self.0.chars(), |acc, c| match c {
+            '(' => acc + 1,
+            ')' => acc - 1,
+            _ => acc
+        });
+
+        reductions.iter().position(|&x| x == -1).unwrap_or(0) as i32 + 1
     }
 }
 
 struct Frequencies {
     frequencies: HashMap<char, u32>,
+}
+
+impl Frequencies {
+    fn get(&self, c: char) -> u32 {
+        *self.frequencies.get(&c).unwrap_or(&0)
+    }
 }
 
 fn frequencies(text: &str) -> Frequencies {
@@ -28,6 +44,23 @@ fn frequencies(text: &str) -> Frequencies {
     });
 
     Frequencies { frequencies }
+}
+
+fn reductions<T, R, F>(init: R, iter: impl IntoIterator<Item = T>, mut func: F) -> Vec<R>
+where
+    T: Clone,
+    R: Clone,
+    F: FnMut(R, T) -> R,
+{
+    let mut results = Vec::new();
+    let mut current = init;
+
+    for item in iter {
+        current = func(current, item);
+        results.push(current.clone());
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -41,6 +74,15 @@ mod tests {
         let directions = Directions::new(input);
 
         assert_eq!(directions.final_floor(), 280);
+    }
+
+    #[test]
+    fn solution_2() {
+        let input = include_str!("../../input/year_2015/day_01/input.txt");
+
+        let directions = Directions::new(input);
+
+        assert_eq!(directions.basement(), 1797);
     }
 
     #[test]
@@ -65,5 +107,30 @@ mod tests {
     fn combinations() {
         let directions = Directions::new("()()(()");
         assert_eq!(directions.final_floor(), 1);
+    }
+
+    #[test]
+    fn test_frequencies() {
+        let frequencies = frequencies("aabddd");
+
+        assert_eq!(frequencies.get('a'), 2);
+        assert_eq!(frequencies.get('b'), 1);
+        assert_eq!(frequencies.get('c'), 0);
+        assert_eq!(frequencies.get('d'), 3);
+    }
+
+    #[test]
+    fn test_reductions() {
+        let directions = reductions(0, vec![1, 1, 1, 1], |acc, x| acc + x);
+        assert_eq!(directions, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn find_basement() {
+        let directions = Directions::new(")");
+        assert_eq!(directions.basement(), 1);
+
+        let directions = Directions::new("()())");
+        assert_eq!(directions.basement(), 5);
     }
 }
